ecf0a4f424ec40b8646aad04debbd8b6973d64a6
dacardenasa <diego.cardenasaleg@gmail.com>
2020-04-08 18:51:59 -0500

    Add spanish i18n global config and devise.es.yml

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, :description, presence: true
-  validates :description, length: { minimum: 250 }
   has_many :comments, :dependent => :destroy
   has_many :users, through: :comments
+  # Validaciones del modelo
+  validates :title, :description, :user_id, presence: true
+  validates :description, length: { minimum: 250 }
+  validates :user_id, numericality: true
+  validates :user_id, numericality: { only_integer: true }
 end
